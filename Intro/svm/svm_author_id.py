9077c0e8e6579db6388320800981c22dc0f3f5f6
Marius Colacioiu <marius.colacioiu@gmail.com>
2017-11-24 22:47:27 +0100

    1% training set, accuracy 0.884

--- Intro/svm/svm_author_id.py
+++ Intro/svm/svm_author_id.py
@@ -25,6 +25,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 clf = svm.SVC(kernel="linear")
 
+features_train = features_train[:len(features_train) / 100]
+labels_train = labels_train[:len(labels_train) / 100]
 clf.fit(features_train, labels_train)
 
 #########################################################
