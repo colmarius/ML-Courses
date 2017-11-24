3e2d3295b011d1aed8e4ed798a4eab735426d6f1
Marius Colacioiu <marius.colacioiu@gmail.com>
2017-11-24 22:46:56 +0100

    Accuracy 0.984

--- Intro/svm/svm_author_id.py
+++ Intro/svm/svm_author_id.py
@@ -1,30 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
-from time import time
 sys.path.append("../tools/")
+
+from time import time
 from email_preprocess import preprocess
+from sklearn import svm
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
-#########################################################
-### your code goes here ###
-
 #########################################################
 
+clf = svm.SVC(kernel="linear")
 
+clf.fit(features_train, labels_train)
+
+#########################################################
+
+accuracy = clf.score(features_test, labels_test)
+
+print("Accuracy {}".format(accuracy))
